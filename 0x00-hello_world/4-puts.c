612fb044af9756b64edd21225b8c9f4107b32065
destinyworld24 <ogunseyewasiu94gmail.com>
2023-02-16 07:16:12 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
  * main- serves as entry point for the program
  * return: By default returns zero if no errors otherwise returns nonzero value
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
