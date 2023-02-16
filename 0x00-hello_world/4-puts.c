bfd6bdf9f2bde0613939362e93a896b84abf4e90
destinyworld24 <ogunseyewasiu94gmail.com>
2023-02-16 08:11:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main- serves as entry point for the program
- * return: By default returns zero if no errors otherwise returns nonzero value
+ * main- Serves as entry point for the program
+ * Return: By default returns zero if no errors otherwise returns nonzero value
  */
 
 int main(void)
@@ -9,3 +9,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
