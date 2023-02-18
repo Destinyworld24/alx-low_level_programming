537c7e15923a22a6184a8db8e9729d67281c4e2e
destinyworld24 <ogunseyewasiu94gmail.com>
2023-02-18 01:59:28 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,10 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main- print the alphabets in lowercase,
  * followed by a new line
  * Return: Always 0 (succcess)
  */
-int main (void)
+int main(void)
 {
 	char ch;
 
