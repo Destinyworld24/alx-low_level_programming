a56ad4ede556347c6244ce5c7b9445cf920696fc
destinyworld24 <ogunseyewasiu94gmail.com>
2023-02-18 01:54:45 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- print the alphabets in lowercase,
+ * followed by a new line
+ * Return: Always 0 (succcess)
+ */
+int main (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
